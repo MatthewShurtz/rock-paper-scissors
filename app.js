fd0b0fa37c2d0539fe9299bc162a037f1adbcbfd
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2021-01-16 13:45:42 -0600

    working on if the user doesn't input RPS

--- app.js
+++ app.js
@@ -11,32 +11,93 @@ const R = "Rock"
 const P = "Paper"
 const S = "Scissors"
 
-// Creating array for user and computer choices
-const playerChoices = [R, P, S];
+// Array for computer choice.
 const computerChoices = [R, P, S];
 
-
-// Welcome and getting players choice
+// Welcome
 let intro = alert("Welcome to Rock, Paper, Scissors");
-let userChoice = prompt("Choose between Rock, Paper, or Scissors");
 
-// Users choice
-userPlay = () => {
-    if (userChoice == "Rock" || userChoice == "rock") {
-        return ("You choose rock")
-    }
-    else if (userChoice == "Paper" || userChoice == "paper") {
-        return ("You choose Paper")
-    }
-    else if (userChoice == "Scissors" || userChoice == "scissors") {
-        return ("You choose Scissors")
-    }
-    else {
-        return ("That's not a choice...")
-    }
-}
+// User chooses between Rock, Paper, and Scissors, then made lower case.
+let userChoice = prompt("Choose between Rock, Paper, or Scissors").toLowerCase();
 
 // Computer randomly selects from choices
 let computerRandomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 let computerChoice = alert("The computer choose " + computerRandomChoice );
+
+// If the user types something other than Rock, Paper, or Scissors.
+nonRPS = () => {
+if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+    alert("That's not a choice O.o");
+}
+}
+
+userNonRPS = nonRPS();
+
+restartGame = () => {
+    if (userNonRPS) {
+      return userChoice
+    }
+}
+
+restartGame()
+// If the user chooses rock.
+rockEval = () => {
+    if (userChoice == "rock" && computerRandomChoice == R) {
+        return ("It's a tie -_-")
+    }
+    else if (userChoice == "rock" && computerRandomChoice == P) {
+        return ("You Lose :-(")
+    }
+    else if (userChoice == "rock" && computerRandomChoice == S) {
+        return ("You Win :-)")
+    }
+}
+
+// If the user chooses paper.
+paperEval = () => {
+    if (userChoice == "paper" && computerRandomChoice == R) {
+        return ("You Win! :-)")
+    }
+    else if (userChoice == "paper" && computerRandomChoice == P) {
+        return ("It's a tie -_-")
+    }
+    else if (userChoice == "paper" && computerRandomChoice == S) {
+        return ("You Lose :-(")
+    }
+}
+
+// If the user chooses scissors.
+scissorsEval = () => {
+    if (userChoice == "scissors" && computerRandomChoice == R) {
+        return ("You Lose :-(")
+    }
+    else if (userChoice == "scissors" && computerRandomChoice == P) {
+        return ("You Win :-)")
+    }
+    else if (userChoice == "scissors" && computerRandomChoice == S) {
+        return ("It's a tie -_-")
+    }
+}
+
+// User choices stored in variable for gameplay.
+userRock = rockEval();
+userPaper = paperEval();
+userScissors = scissorsEval();
+
+// Game is played.
+gamePlay = () => {
+    if (userRock) {
+        alert(userRock)
+    }
+    else if (userPaper) {
+        alert(userPaper)
+    }
+    else if (userScissors) {
+        alert(userScissors)
+    }
+}
+
+// console.log(userChoice);
+// console.log(gamePlay());
+
 
